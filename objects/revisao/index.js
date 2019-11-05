2f0a3c8bcd78b32831b7eeac46d62f053639057a
Priscilla Alves <Priihalves_@hotmail.com>
2019-11-05 13:24:32 -0300

    exemplos de new object

---
+++ objects/revisao/index.js
@@ -0,0 +1,59 @@
+// // func procedimento(dado1, dado2){
+// //     //barata
+
+// // }
+
+// // obj={
+// //     dados1,
+// //     dados2,
+// //     dados3
+// //     procedimento(){
+// //         //...
+// //     }
+// // }
+
+// const criança = new object
+// console.log(typeof criança)
+// crianca.nome = "Elaine"
+// crianca.idade = 5
+// crianca['brinquedo favorito'] = "Homem aranha"
+// console.log(crianca["idade"])
+// console.log(crianca["brinquedo favorito"])
+// delete.crianca.idade
+
+
+// const crianca2 = {
+//     nome: "Paulo",
+//     idade: 6,
+//     endereco: {
+//         logradouro: "Rua dos bobos",
+//         numero: 0,
+//         referencia: [
+//             endereco: {
+//                 logradouro: "Rua dos bobos",
+//                 numero: 2,
+//                 descricao: "Mercado"
+//             }
+//         }
+//     ]
+// },
+// responsaveis
+
+
+function Professor (nome,sobrenome,materia){
+    this.nome= nome
+    this.sobrenome = sobrenome
+    this.materia= materia
+}
+
+
+const c1 = new Professor("Bete","Faria", "Português")
+console.log(Professor)
+
+
+
+
+
+
+
+
